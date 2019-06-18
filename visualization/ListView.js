601f366685984288c10d273ee452c491650ee48b
Unknown <justataya@gmail.com>
2019-06-18 13:39:05 +0300

    Progress commit

--- visualization/ListView.js
+++ visualization/ListView.js
@@ -6,25 +6,39 @@ class ListView {
 
     addNode_begin(value) {
         this._list.addNode_begin(value);
+        return this.update();
     }
 
     addNode_end(value) {
         this._list.addNode_end(value);
+        return this.update();
     }
 
     addNode_before(value, position) {
         this._list.addNode_after(value,position);
+        return this.update();
     }
+
 
     deleteNode(value){
         this.deleteNode(value);
+        return this.update();
     }
 
     searchNode(value){
-        
+        let node=this._list.searchNode(value);
+        let li=new ListIterator(this._list);
+        let index=0;
+        while(!li.end()){
+            if(li.current==node){
+                return index;
+            }
+            index++;
+        }
     }
 
     update(){
         this._nodes=getListArray(this._list);
+        return this._nodes;
     }
 }
